2593f44a8446fff845e4bab96149d440b26573f9
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2014-09-15 16:25:48 +0100

    changed instr to instructor in instructors.client.controller.js

--- public/modules/instructors/controllers/instructors.client.controller.js
+++ public/modules/instructors/controllers/instructors.client.controller.js
@@ -11,20 +11,13 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			$http.post('/auth/signin', $scope.credentials).success(function(response) {
 				//If successful we assign the response to the global user model
 				$scope.user = response;
-				console.log('signed');
-
 				//And redirect to the right page
-				console.log($scope.user);
-				console.log($scope.credentials);
-
 				if ($scope.user.role === 'instructor') {
-					$location.path('/instructors/home');
-					
+					$location.path('/instructors/home');	
 				}
 				else{
 					console.log('instructorsigin');
 					$location.path('/');
-
 				}
 			}).error(function(response) {
 				$scope.error = response.message;
@@ -32,45 +25,38 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			});
 		};
 
+
 		$scope.instructorHome = function(){
-			console.log(Authentication.user);
 			if (!$scope.user) {
 					$location.path('/');
 			}
-			
 		};
 
 		// for bootcamps
 		$scope.listBootcamps = function() {
 
 			$scope.appl_length= 0;
-		  	$http.get('/instr/bootcamps').success(function(response) {
+		  	$http.get('/instructor/bootcamps').success(function(response) {
 		    // If successful show success message and clear form
 		    	$scope.success = true;
 		    	$scope.bootcamps = response;
 		  	}).error(function(response) {
+                console.log('error');
 		    $scope.error = response.message;
 		  	});
 		};
 
-		// if $locaton.path('/')
-
-
 		$scope.viewBootcamp = function() {
-			console.log($stateParams.bootcampId.length);
 			if ($stateParams.bootcampId.length < 20) {
 				$location.path('/');
 			}else{
-			
-			  	$http.get('/instr/camp/' + $stateParams.bootcampId).success(function(response) {
+			  	$http.get('/instructor/camp/' + $stateParams.bootcampId).success(function(response) {
 			    // If successful show success message and clear form
-			    	console.log('/instr/camp/' + $stateParams.bootcampId);
 			    	$scope.success = true;
 			    	$scope.bootcamp = response;
-			    	console.log($scope.bootcamp);
 			    	$scope.applicants = $scope.bootcamp.applicants;
-			    	// console.log($scope.applicants);
 				}).error(function(response) {
+                    console.log('error');
 				    $scope.error = response.message;
 				}); 
 			}
@@ -80,18 +66,13 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 		// for trainees
 		$scope.listTrainees = function() {
 			console.log('inside listcontroller');
-		  	$http.get('/instr').success(function(response) {
+		  	$http.get('/instructor').success(function(response) {
 		    // If successful show success message and clear form
-		    // $scope.user;
-		    console.log('working list');
 		    	$scope.success = true;
-
-		    	$scope.trainees = response;
-		    	console.log($scope.trainees);
-																																																				    	// $location.path('instructors/applicants');
+		    	$scope.trainees = response;																		
 		  }).error(function(response) {
+            console.log('error');
 		    $scope.error = response.message;
-
 		  	});
 		};
 
@@ -100,25 +81,17 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			if ($stateParams.applicantId.length < 20) {
 				$location.path('/');
 			}else{
-			
 				$rootScope.traineeId = $stateParams.applicantId;
-				$http.get('/instr/trainee/' + $rootScope.traineeId).success(function(response) {
+				$http.get('/instructor/trainee/' + $rootScope.traineeId).success(function(response) {
 			    // If successful show success message and clear form
 			    	$scope.success = true;
 			    	$rootScope.trainee = response;
-		
 			    	$rootScope.assessments = $rootScope.trainee.assessments;   	    	
 			    	angular.forEach($rootScope.trainee.assessments, function(assessment, key){
 			    		$rootScope.assessment = assessment; 
-			    		console.log(assessment); 
-			    		console.log(user._id); 
-			    		console.log(assessment.instructorId);
 			    	});
-			 
-			    	// $location.path('instructors' + applicant._id);
 			  	}).error(function(response) {
 			    $scope.error = response.message;
-
 			  	});
 			}
 		};
@@ -133,15 +106,8 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 				score: this.score
 			});
 
-			console.log($scope.assessment);
-			// console.log($stateParams);
-			// console.log($scope.assessment.assessment_name);
-			
-				// $scope.enterNew = false;
-			$http.post('/instr/trainee/'+ $stateParams.applicantId, $scope.assessment).success(function(response) {
-				
+			$http.post('/instructor/trainee/'+ $stateParams.applicantId, $scope.assessment).success(function(response) {
 				$location.path('/instructors/trainees/' + $stateParams.applicantId);
-				
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
@@ -149,18 +115,15 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			this.assessment_name = '';
 			this.assessment_date = '';
 			this.score = '';
-			  	console.log(typeof assessment_date);
 		};
 
 		$rootScope.editAssessment = function(index, assessment) {
 			$rootScope.assessment = assessment;
 			$rootScope.index = index;
-			console.log($rootScope.assessment);
 		};
 
 		$scope.updateAssessment = function() {
-			console.log($scope.index);
-			$http.put('/instr/trainee/' + $scope.assessment.applicantId + '/' + $scope.assessment._id,  $scope.assessment).success(function(response) {
+			$http.put('/instructor/trainee/' + $scope.assessment.applicantId + '/' + $scope.assessment._id,  $scope.assessment).success(function(response) {
 		    // If successful show success message and clear form
 		    $location.path('/instructors/trainees/' + $scope.assessment.applicantId);
 		  	}).error(function(response) {
@@ -171,41 +134,19 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 		$scope.deleteAssessment = function(index, assessment) {
 
 			$scope.assessments.splice(index, 1);
-			console.log('response');
-		  	$http.delete('/instr/trainee/'+ $stateParams.applicantId +'/'+ assessment._id).success(function(response) {
-		 		console.log('response');
+		  	$http.delete('/instructor/trainee/'+ $stateParams.applicantId +'/'+ assessment._id).success(function(response) {
 		 	    $scope.success = true;
-		 	    console.log("responsem");
 			}).error(function(response) {
 			   	$scope.error = response.message;
 			});   
 		};
- 
-
-		// $scope.selectFellow = function() {
-		// 	console.log($scope.trainee.role);
-		// 	$scope.trainee.role = "fellow";
-		// 	console.log($scope.trainee);
-			
-		//   	$http.put('/instr/trainee/' + $stateParams.applicantId, $scope.trainee).success(function(response) {
-		//     // If successful show success message and clear form
-		    
-		//     	$scope.success = true;
-		//     	$location.path('instructors/fellow_selected/' + $scope.traineeId);
-		//   	}).error(function(response) {
-		//     $scope.error = response.message;
-		//   	});
-		// };
-		
 
 		// for fellows
 		$scope.listFellows = function() {
-		  	$http.get('/instr/fellows').success(function(response) {
+		  	$http.get('/instructor/fellows').success(function(response) {
 		    // If successful show success message and clear form
 		    	$scope.success = true;
 		    	$scope.fellows = response;
-		    	console.log($scope.fellows);
-		    	// $location.path('instructors'...);
 		  	}).error(function(response) {
 		    $scope.error = response.message;
 
@@ -217,85 +158,52 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			if ($stateParams.fellowId.length < 20) {
 				$location.path('/');
 			}else{
-				$http.get('/instr/trainee/' + $stateParams.fellowId).success(function(response) {
+				$http.get('/instructor/trainee/' + $stateParams.fellowId).success(function(response) {
 			    // If successful show success message and clear form
 			    	$scope.success = true;
 			    	$scope.fellow = response;
 			    	angular.forEach($scope.fellow.skillSets, function(skillSets, key){
 			    		$scope.skillSets = skillSets;  
-			    		console.log(user._id); 
-			    		console.log($scope.fellow);
-			    	});
-			    	
-			    	// $location.path('instructors' + applicant._id);
+			    	}); 	
 			  	}).error(function(response) {
 			    $scope.error = response.message;
-
 			  	});
 			}
 		};
 
 
 		$scope.ratefellow = function() {
-	      //Permissions check
-	      console.log($stateParams);
-	      console.log($scope.data);
-	        $http.post('/instr/trainee/' + $stateParams.fellowId + '/rate', $scope.data).success(function(response) {
-	        // If successful show success message and clear form
+	        $http.post('/instructor/trainee/' + $stateParams.fellowId + '/rate', $scope.data).success(function(response) {
 	        $scope.success = true;
-	        console.log('Success - Done', response + $stateParams.fellowId);
 	        $location.path('/instructors/fellows/' + $stateParams.fellowId);
-	        
 	      	}).error(function(response) {
 	        $scope.error = response.message;
-	        console.log('Error - can not');
 	      	});
 	    };
 
 
-		
 		$scope.deleteRate = function(index, skillSet) {
 			$scope.skillSets = $scope.fellow.skillSets;
 			$scope.skillSets.splice(index, 1);
-		  	$http.delete('/instr/trainee/'+ $scope.fellow._id +'/rate/'+ skillSet._id).success(function(response) {
-		 		console.log('response');
+		  	$http.delete('/instructor/trainee/'+ $scope.fellow._id +'/rate/'+ skillSet._id).success(function(response) {
 		 	    $scope.success = true;
-		 	    console.log("responsem");
 			}).error(function(response) {
+                console.log('error');
 			   	$scope.error = response.message;
 			});   
 		};
 
-		// $rootScope.editRate= function(index, skillSets) {
-		// 	$rootScope.skillSets = skillSets;
-		// 	$rootScope.index = index;
-		// 	console.log($rootScope.skillSets);
-		// };
-
-		// $scope.editingRate = function(){
-		// 	$scope.skillSetsNow = $scope.skillSets;
-		// 	console.log($scope.skillSetsNow);
-
-		// };
 		
 		$scope.editFellowRate= function(){
-		console.log($scope.skillSets);
-		console.log($stateParams);
-
-			$http.put('/instr/trainee/' + $stateParams.fellowId + '/rate/' + $stateParams.skillSetId, $scope.skillSetsNow).success(function(response) {
+			$http.put('/instructor/trainee/' + $stateParams.fellowId + '/rate/' + $stateParams.skillSetId, $scope.skillSetsNow).success(function(response) {
 		    // If successful show success message and clear form
 		    	$scope.success = true;
-		    	console.log('success');
-		    	// console.log();
 		    	$location.path('/instructors/fellows/' + $stateParams.fellowId);
 		  	}).error(function(response) {
 		    $scope.error = response.message;
-
 		  	});
-
 		};
 		
-
 
  // Upload Image
 	    $scope.onFileSelect = function($file) {
@@ -309,6 +217,7 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 			}
 	    };
 
+
 	    $scope.removeAlert = function(message) {
             if (message === "error") {
                 $scope.error = null;
@@ -319,12 +228,11 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 
 
         // upload file
-		$scope.create = function() {
-			console.log($scope.details + $scope.details.exp);
+		$scope.create = function() {;
 			$scope.success = null;
 			$scope.error = null;
 			$scope.upload = $upload.upload({
-	            url: '/instr/updateInfo',
+	            url: '/instructor/updateInfo',
 	            method: 'POST',
 	            data: $scope.details,
 	            file: $scope.file
@@ -332,36 +240,30 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 	        	// $scope.instr = response;
 	            $scope.success = 'Your details have been updated successfully';
 	        }).error(function(err) {
-	        	$scope.error = err.message;
-	            console.log('Error uploading file: ' + err.message || err);
+	        	$scope.error = err.message;;
 	        });
 		};
 
+
 		$scope.deletePhoto = function($index, photo){
 			$scope.photo = $scope.user.photo;
-			$scope.user.photo = "";
-			console.log($scope.photo);
-			//  $scope.delete_image = !$scope.delete_image;	
-			// console.log( $scope.delete_image);	
-			$http.delete('/instr/' + $scope.user._id + '/deletePhoto').success(function(response){
+			$scope.user.photo = "";	
+			$http.delete('/instructor/' + $scope.user._id + '/deletePhoto').success(function(response){
 				 $scope.success = true;
 		 		$scope.photo = response;
-
 		 		$scope.upload_new=true;
 			}).error(function(response) {
 			   	$scope.error = response.message;
 
 			});
-
 		};
 
 
-		 $scope.showImage = function(img) {
+		$scope.showImage = function(img) {
 		 	if (img) {
                img = img.substring(6);
             	return img;
-		 	}
-            
+		 	}    
         };
 
 
@@ -371,74 +273,28 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 		    if ($scope.user === null){
 		        $location.path('/');
 		    }
-		    
 		    else if ($scope.user.role !== 'instructor'){
 		        $location.path('/');
 		    }
 
-		    $http.post('/instr/skill', $scope.data).success(function(response) {
+		    $http.post('/instructor/skill', $scope.data).success(function(response) {
 		        // If successful show success message and clear form
 		        $scope.success = true;
-		        console.log('Success - Done', response);
-		        console.log($scope.data);
 		        $location.path('/instructors/home');
 		        
 		    }).error(function(response) {
 		        $scope.error = response.message;
-		        console.log('Error - can not');
 		    });
-	      
 	    };
-
-	    $scope.editingSkill = function() {
-	    	console.log($stateParams);
-	    };
-	    // '/instr/skill/:userId/:skillId'
-
-		
-		$scope.editSkill = function(){
-		// 	$http.put('/instr/skill/:userId/:skillId').success(function(response) {
-		//     // If successful show success message and clear form
-		//     	$scope.success = true;
-		//     	// $location.path('instructors'...);
-		//   	}).error(function(response) {
-		//     $scope.error = response.message;
-
-		//   	});
-
-		};
-
-
-		$scope.deleteSkill = function($index, skillSet){
-			console.log($stateParams);
-			// $scope.skillSet.splice(index, 1);
-
-		};
-
-		// $scope.deleteRating = function(){
-		// 	$scope.success = $scope.error = null;
-
-		// 	$http.delete('/instr/skill/:userId/:skillId').success(function(response) {
-		//     // If successful show success message and clear form
-		//     	$scope.success = true;
-		//     	// $location.path('instructors'...);
-		//   	}).error(function(response) {
-		//     $scope.error = response.message;
-
-		//   	});
-
-		// };
-
 
 
 	    $scope.today = function() {
 	    	$scope.dt = new Date();
-	    	// $scope.dt = assessment_date;
-	    	console.log(typeof $scope.dt);
-	    	console.log($scope.dt);
 	    };
+
 		
 		$scope.today();
+
 
 		$scope.clear = function () {
 		    $scope.dt = null;
@@ -450,11 +306,14 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 		    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
 		};
 
+
 		$scope.toggleMin = function() {
 		    $scope.minDate = $scope.minDate ? null : new Date();
 		};
+
 		  
 		$scope.toggleMin();
+
 
 		$scope.open = function($event) {
 		    $event.preventDefault();
@@ -463,20 +322,18 @@ angular.module('instructors').controller('InstructorsController', ['$scope', '$r
 		    $scope.opened = true;
 		};
 
+
 		$scope.dateOptions = {
 		    formatYear: 'yy',
 		    startingDay: 1
 
 		};
 
-		// $scope.initDate = new Date('2016-15-20');
+
 		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate', 'longDate'];
 		$scope.format = $scope.formats[4];
 		
-		 // assessment_date: "2014-12-09T23:00:00.000Z
-		
 	}
-	
 	
 ]);
 
